2d2438c9c32a37c24f54287ce163e60dfc13e2fe
AlissonCouto <alissonvieiracaires@gmail.com>
2025-11-05 22:55:56 -0400

    feat(ECM-0002): IMplementação do layou base para os perfis e administrador e de cliente logado.

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,21 +1,125 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+@php
+
+use Illuminate\Support\Facades\Auth;
+
+$companyId = 1;
+
+$page = url()->current();
+
+$menu = [
+
+[
+'name' => 'Home',
+'icon' => '<i class="mdi mdi-home icon"></i>',
+'link' => route('admin.dashboard')
+],
+[
+'name' => 'Leads',
+'icon' => '<i class="mdi mdi-account-group icon"></i>',
+'link' => '#'
+],
+[
+'name' => 'Whatsapp',
+'icon' => '<i class="mdi mdi-whatsapp icon"></i>',
+'link' => '#'
+],
+[
+'name' => 'Status',
+'icon' => '<i class="mdi mdi-chart-timeline icon"></i>',
+'link' => '#'
+],
+[
+'name' => 'Nichos',
+'icon' => '<i class="mdi mdi-layers icon"></i>',
+'link' => '#'
+],
+[
+'name' => 'Finanças',
+'icon' => '<i class="mdi mdi-finance icon"></i>',
+'link' => '#'
+],
+[
+'name' => 'Configurar',
+'icon' => '<i class="mdi mdi-cog icon"></i>',
+'link' => '#'
+],
+
+];
+
+@endphp
+
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <title>@yield('title') - Admin</title>
-    @vite('resources/css/app.css')
-    @vite('resources/js/app.js')
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    @auth
+    <meta name="company-id" content="{{ $companyId }}">
+    @endauth
+
+    <title>{{ config('app.name', 'Laravel') }}</title>
+
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@5.x.x/css/materialdesignicons.min.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet">
+
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/css/lightbox.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+    <!-- Styles -->
+    <!-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> -->
+    @vite(['resources/css/admin.css'])
+
 </head>
 
-<body class="bg-gray-100">
-    <nav>
-        <!-- Menu lateral do admin -->
-    </nav>
+<body>
 
-    <main class="p-4">
-        @yield('admin-content')
-    </main>
+    <div class="app-container">
+
+        <div class="sidebar">
+
+            <div class="menu">
+
+                <div class="logo">
+                    C
+                </div>
+
+                @foreach($menu as $k => $item)
+                <div class="menu-item {{ $page == $item['link'] ? '-active' : '' }}">
+                    <a href="{{$item['link']}}" class="menu-link">
+                        {!!$item['icon']!!}
+                        <span class="name">{{$item['name']}}</span>
+                    </a>
+                </div>
+                @endforeach
+
+            </div>
+
+        </div>
+
+        <div class="workspace">
+
+            @yield('content')
+
+        </div>
+
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+    <!--
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lightbox2/2.11.1/js/lightbox.min.js"></script>
+    -->
+
 </body>
 
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Admin\AdminController;
+use App\Http\Controllers\Profile\ProfileController;
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+/* Rotas da área administrativa do adminiistrador */
+Route::prefix('/admin')->group(function () {
+    Route::get('dashboard', [AdminController::class, 'dashboard'])->name('admin.dashboard');
+});
+/* Rotas da área administrativa do adminiistrador */
+
+/* Rotas da área administrativa do cliente */
+Route::prefix('/perfil')->group(function () {
+    Route::get('/', [ProfileController::class, 'dashboard'])->name('profile.dashboard');
+});
+/* Rotas da área administrativa do cliente */
+
+/* Rotas públicas do site */
